815c5a6c947b42816be5f44bf21bf59322d9f599
aetos <aetos382@users.noreply.github.com>
2026-04-20 14:48:29 +0900

    completions

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,15 @@ set -euo pipefail
 git lfs install
 
 curl -fqsLS https://claude.ai/install.sh | bash
+
+pwsh -NoProfile -NonInteractive -Command - <<'EOS'
+$profilePath = $profile.CurrentUserAllHosts
+$profileDirectory = $profilePath | Split-Path -Parent
+
+New-Item -ItemType Directory -Force -Path $profileDirectory > $null
+
+$completionPath = $profileDirectory | Join-Path -ChildPath hugo-completions.ps1
+
+hugo completion powershell > $completionPath
+". ${completionPath}" >> $profilePath
+EOS
